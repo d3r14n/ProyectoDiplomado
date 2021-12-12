b2ea13c07460461a9c1d5d5a49fd0cbc57005187
Derian J. Hndz. L <nairedriaj@hotmail.com>
2021-12-11 22:49:57 -0600

    feat:sentry

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const Sentry = require('@sentry/node');
+const Tracing = require('@sentry/tracing');
 
 var indexRouter = require('./routes/index');
 var tasksRouter = require('./routes/tasks');
@@ -10,8 +12,27 @@ var paymentRouter = require('./routes/payment');
 
 var app = express();
 
+Sentry.init({
+    dsn: "https://ea7f93daef9c41d2bf2797106a43dc3b@o1059722.ingest.sentry.io/6103729",
+    integrations: [
+        new Sentry.Integrations.Http({ tracing: true }),
+        new Tracing.Integrations.Express({ app }),
+      ],
+    tracesSampleRate: 1.0,
+});
+
 // view engine setup
 app.set('view engine', 'jade');
+
+app.use(Sentry.Handlers.requestHandler());
+app.use(Sentry.Handlers.tracingHandler());
+
+app.use(Sentry.Handlers.errorHandler());
+
+app.use(function onError(err, req, res, next){
+    res.statusCode = 500;
+    res.end(res.sentry + "\n");
+});
 
 app.use(logger('dev'));
 app.use(express.json());
